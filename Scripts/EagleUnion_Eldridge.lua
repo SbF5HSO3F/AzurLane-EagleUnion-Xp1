0df68b8e2e6abba701d59f72cb157c5abfd10c4e
HSbF5HSO3F <jinghaoyu1030@qq.com>
2024-12-25 08:21:02 +0800

    埃尔德里奇：更新埃尔德里奇的能力和描述信息优化用户界面功能

--- Scripts/EagleUnion_Eldridge.lua
+++ Scripts/EagleUnion_Eldridge.lua
@@ -5,5 +5,32 @@
 --||=======================include========================||--
 include('EagleUnion_Core.lua')
 
+--||=================GameEvents functions=================||--
+
+--transmission unit
+function EldridgePlaceUnit(playerID, param)
+    local pPlayer = Players[playerID]
+    if pPlayer then
+        local pUnit = UnitManager.GetUnit(playerID, param.unitID)
+        if pUnit then
+            UnitManager.PlaceUnit(pUnit, param.x, param.y)
+        end
+    end
+end
+
+--||======================initialize======================||--
+
+--initialization function
+function Initialize()
+    -----------------------Events-----------------------
+
+    ---------------------GameEvents---------------------
+    GameEvents.EldridgeRePlaceUnit.Add(EldridgePlaceUnit)
+    ----------------------------------------------------
+    ----------------------------------------------------
+    print('Initial success!')
+end
 
 include('EagleUnion_Eldridge_', true)
+
+Initialize()
--- UI/Additions/EagleUnion_EldridgeUnitUI.lua
+++ UI/Additions/EagleUnion_EldridgeUnitUI.lua
@@ -13,6 +13,37 @@ local m_EldridgeSelected, m_EldridgePlot = false, nil
 --||======================MetaTable=======================||--
 
 EldridgeUnitPanel = {
+    --重置
+    Reset = function(self)
+        --get the unit
+        local pUnit = UI.GetHeadSelectedUnit()
+        --check the leader is Eldridge
+        if EagleCore.CheckLeaderMatched(
+                Game.GetLocalPlayer(), 'LEADER_ELDRIDGE_DE173'
+            ) and pUnit
+        then
+            Controls.EldridgeGrid:SetHide(false)
+            --reset the buttons
+            self.Rainbow:Reset(pUnit)
+        else
+            --hide the grid
+            Controls.EldridgeGrid:SetHide(true)
+        end
+        --reset the Unit Panel
+        ContextPtr:LookUpControl("/InGame/UnitPanel"):RequestRefresh()
+    end,
+    --初始化
+    Init = function(self)
+        local context = ContextPtr:LookUpControl("/InGame/UnitPanel/StandardActionsStack")
+        if context then
+            --change the parent
+            Controls.EldridgeGrid:ChangeParent(context)
+            --Register Callback
+            self.Rainbow:Register()
+            --reset the button
+            self:Reset()
+        end
+    end,
     --彩虹计划：传送
     Rainbow = {
         GetPlots = function(pUnit)
@@ -64,7 +95,7 @@ EldridgeUnitPanel = {
                 UI.SetInterfaceMode(InterfaceModeTypes.SELECTION)
                 UI.SetInterfaceMode(InterfaceModeTypes.WB_SELECT_PLOT)
                 local plots, hash = self.GetPlots(pUnit)
-                m_SeydlitzPlot = hash
+                m_EldridgePlot = hash
                 if #plots > 0 then
                     UILens.SetLayerHexesArea(COLOR, Game.GetLocalPlayer(), plots)
                     UILens.ToggleLayerOn(COLOR)
@@ -74,6 +105,44 @@ EldridgeUnitPanel = {
             end
             --set the button selected state
             Controls.Rainbow:SetSelected(state)
+        end,
+        --获取细节
+        GetDetail = function(self, pUnit)
+            local detail = { Disable = true, Reason = 'NONE' }
+            if pUnit:GetMovesRemaining() == 0 then
+                --no movement, disabled
+                detail.Reason = Locale.Lookup('LOC_UNITCOMMAND_RAINBOW_NOMOVEMENT')
+            else
+                local plots = self.GetPlots(pUnit)
+                if plots and #plots > 0 then
+                    detail.Disable = false
+                else
+                    --no plot, disabled
+                    detail.Reason = Locale.Lookup('LOC_UNITCOMMAND_RAINBOW_NOPLOT')
+                end
+            end
+            --return the button detail
+            return detail
+        end,
+        --设置按钮
+        Reset = function(self, pUnit)
+            --get the button details
+            local detail = self:GetDetail(pUnit)
+            --get the disable
+            local disable = detail.Disable
+            --set the button
+            Controls.Rainbow:SetDisabled(disable)
+            Controls.Rainbow:SetAlpha((disable and 0.7) or 1)
+            --the tooltip
+            local tooltip = Locale.Lookup('LOC_RAINBOW_TITLE') ..
+                '[NEWLINE][NEWLINE]' .. Locale.Lookup('LOC_RAINBOW_DESC')
+            if disable then
+                tooltip = tooltip .. '[NEWLINE][NEWLINE]' .. detail.Reason
+                --quit the mode
+                self.Quit(false)
+            end
+            --set the tooltip
+            Controls.Rainbow:SetToolTipString(tooltip)
         end,
         --回调函数
         Callback = function(self)
@@ -85,6 +154,11 @@ EldridgeUnitPanel = {
                 --set the button state
                 self:SetState(m_EldridgeSelected, pUnit)
             end
+        end,
+        --注册函数
+        Register = function(self)
+            Controls.Rainbow:RegisterCallback(Mouse.eLClick, function() self:Callback() end)
+            Controls.Rainbow:RegisterCallback(Mouse.eMouseEnter, EagleUnionEnter())
         end
     },
     --电气功率：电击
@@ -95,5 +169,88 @@ EldridgeUnitPanel = {
 
 --||====================base functions====================||--
 
+--||===================Events functions===================||--
+
+--When the unit is selected
+function EldridgeOnUnitSelectChanged(playerId, unitId, locationX, locationY, locationZ, isSelected, isEditable)
+    if isSelected and playerId == Game.GetLocalPlayer() then
+        EldridgeUnitPanel:Reset()
+    end
+end
+
+--On ui mode change
+function EldridgeUIModeChange(intPara, currentInterfaceMode)
+    if m_EldridgeSelected and currentInterfaceMode ~= InterfaceModeTypes.WB_SELECT_PLOT then
+        EldridgeUnitPanel.Rainbow.Quit(false)
+    end
+end
+
+--Add a button to Unit Panel
+function EldridgeOnLoadGameViewStateDone()
+    EldridgeUnitPanel:Init()
+end
+
+--reset the grid
+function EldridgeGridReset()
+    EldridgeUnitPanel:Reset()
+end
+
+--||=================LuaEvents functions==================||--
+
+--On selected plot
+function EldridgeSelectPlot(plotID, edge, lbutton, rbutton)
+    if lbutton then return end
+    if rbutton then
+        EldridgeUnitPanel.Rainbow.Quit(true)
+    else
+        local pUnit = UI.GetHeadSelectedUnit()
+        if pUnit and m_EldridgePlot and m_EldridgePlot[plotID] == 1 then
+            EldridgeUnitPanel.Rainbow.Quit(true)
+            UI.RequestPlayerOperation(Game.GetLocalPlayer(),
+                PlayerOperations.EXECUTE_SCRIPT, {
+                    OnStart = 'EldridgeRePlaceUnit',
+                    unitID = pUnit:GetID(),
+                    x = Map.GetPlotByIndex(plotID):GetX(),
+                    y = Map.GetPlotByIndex(plotID):GetY(),
+                }
+            ); m_EldridgePlot = nil; UI.PlaySound("Unit_Relocate")
+        end
+    end
+end
+
+--||======================initialize======================||--
+
+--Initialize
+function Initialize()
+    Events.LoadGameViewStateDone.Add(EldridgeOnLoadGameViewStateDone)
+    Events.UnitSelectionChanged.Add(EldridgeOnUnitSelectChanged)
+    --Events.UnitActivate.Add(SeydlitzUnitActive)
+    ------------------------------------------
+    Events.UnitOperationSegmentComplete.Add(EldridgeGridReset)
+    Events.UnitCommandStarted.Add(EldridgeGridReset)
+    Events.UnitDamageChanged.Add(EldridgeGridReset)
+    Events.UnitMoveComplete.Add(EldridgeGridReset)
+    Events.UnitChargesChanged.Add(EldridgeGridReset)
+    Events.UnitPromoted.Add(EldridgeGridReset)
+    Events.UnitOperationsCleared.Add(EldridgeGridReset)
+    Events.UnitOperationAdded.Add(EldridgeGridReset)
+    Events.UnitOperationDeactivated.Add(EldridgeGridReset)
+    Events.UnitMovementPointsChanged.Add(EldridgeGridReset)
+    Events.UnitMovementPointsCleared.Add(EldridgeGridReset)
+    Events.UnitMovementPointsRestored.Add(EldridgeGridReset)
+    Events.UnitAbilityLost.Add(EldridgeGridReset)
+
+    Events.CityAddedToMap.Add(EldridgeGridReset)
+    Events.CityRemovedFromMap.Add(EldridgeGridReset)
+    ------------------------------------------
+    Events.PhaseBegin.Add(EldridgeGridReset)
+    Events.InterfaceModeChanged.Add(EldridgeUIModeChange)
+    ------------------------------------------
+    LuaEvents.WorldInput_WBSelectPlot.Add(EldridgeSelectPlot)
+    ------------------------------------------
+    print('Initial success!')
+end
 
 include('EagleUnion_EldridgeUnitUI_', true)
+
+Initialize()
